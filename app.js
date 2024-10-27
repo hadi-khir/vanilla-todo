d187a8acbbdcd8c06cb299c1274a4874af86524f
hadi khir <khir.hadi@gmail.com>
2024-10-26 21:04:54 -0400

    adding js functionality for item components

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+/**
+ * Creates a new HTML todo item, include the li parent node, along with the input, span and button child elements.
+ * @param {HTMLEvent} event signalling the entering of a todo item via return key click.
+ * @returns a new @code{'li'} item to be added to the task list. 
+ */
 function createTodo(event) {
-    if (event.key !== 'Enter') {
-        return false;
-    }
+
+    if (event !== null && event !== undefined) {
+        
+        if (event.key !== "Enter") {
+            return false;
+        }
+    } 
 
     // Grab the user input. 
     const newTodoInput = document.querySelector('.todo-input').value;
@@ -14,6 +23,7 @@ function createTodo(event) {
     var deleteBtn = document.createElement('button');
     deleteBtn.appendChild(document.createTextNode('Delete'));
     deleteBtn.classList.add('todo-item-delete');
+    deleteBtn.setAttribute('onclick', 'deleteTodo(event)')
     
     // Create the title
     var todoTitle = document.createElement('span');
@@ -23,6 +33,7 @@ function createTodo(event) {
     // Create the input checkbox
     var todoCheckBox = document.createElement('input');
     todoCheckBox.setAttribute('type', 'checkbox');
+    todoCheckBox.setAttribute('onclick', 'completeTodo(event)');
     todoCheckBox.classList.add('todo-item-checkbox');
 
     // Add the children to the list item
@@ -39,8 +50,53 @@ function createTodo(event) {
     // Reset the input
     document.querySelector('.todo-input').value = '';
 
-    event.stopPropagation();
-    event.preventDefault();
+    if (event) {
+
+        event.stopPropagation();
+        event.preventDefault();
+    }
+}
+
+/**
+ * Removes an item from the todo list.
+ * @param {HTMLEvent} event signaling the onclick function of the todo delete button.
+ * @returns false if the event is null or undefined. Nothing otherwise.
+ */
+function deleteTodo(event) {
+
+    if (event === null || event === undefined) {
+        alert('Something went wrong, could not process request.');
+        return false;
+    }
+
+    let selectedTodo = event.target;
+    let parentEL = selectedTodo.parentNode;
+    parentEL.remove();
+}
+
+/**
+ * Completes a todo item by applying a CSS class to the span element. 
+ * @param {HTMLEvent} event actioning the checkbox click of a todo item. 
+ * @returns false if the event is null, signaling an error. Nothing otherwise.
+ */
+function completeTodo(event) {
+
+    if (event === null || event === undefined) {
+        alert('Something went wrong, could not process request.');
+        return false;
+    }
+
+    let checkbox = event.target;
+    let parent = checkbox.parentNode;
+    let todoItemText = parent.querySelector('span');
+
+    if (checkbox.checked === true) {
+
+        todoItemText.classList.add('list-item-completed');
+    } else {
+
+        todoItemText.classList.remove('list-item-completed');
+    }
 }
 
 /**
@@ -51,7 +107,6 @@ function createTodo(event) {
  */
 function notNullOrEmpty(inputStr) {
 
-    console.log(inputStr);
     const trimmedInput = inputStr.trim();
     if (trimmedInput !== null && trimmedInput !== '' && trimmedInput.length > 0) {
         return true;
